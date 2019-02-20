873a8f01e470eded5b21607fcdf81c875cde292d
Adesuyi Ayodeji <28259980+Scarecrow94@users.noreply.github.com>
2019-02-20 09:05:35 +0100

    FAQ fixed

--- app/src/main/java/com/blackviking/hosh/Settings/Faq.java
+++ app/src/main/java/com/blackviking/hosh/Settings/Faq.java
@@ -81,73 +81,73 @@ public class Faq extends AppCompatActivity {
 
         listFaqTitle.add("SIGNING UP");
         listFaqTitle.add("LOGGING IN");
-        listFaqTitle.add("FORGOTTEN PASSWORD");
-        listFaqTitle.add("HOME PAGE");
-        listFaqTitle.add("COURSE OPTIONS");
-        listFaqTitle.add("DOWNLOADED FILES");
-        listFaqTitle.add("CONTACT US");
+        listFaqTitle.add("HOME");
+        listFaqTitle.add("HOSH FEED");
+        listFaqTitle.add("HOOKUP");
+        listFaqTitle.add("HOPDATE");
+        listFaqTitle.add("MESSAGING");
+        listFaqTitle.add("ACCOUNT");
 
 
         List<String> signUp = new ArrayList<>();
-        signUp.add("Users are required to sign up with their Google Certified Mail");
-        signUp.add("Step 1 : Click on the Google Sign In button below the logo");
-        signUp.add("Step 2 : Select Account you want as your personal login Account");
-        signUp.add("Step 3 : Fill in the details required by the app in the next page.");
+        signUp.add("There are 3 sign up options");
+        signUp.add("New users can sign up with either their google account, valid email address or anonymously");
+        signUp.add("Google Step 1 : Click on the Google Sign In button below the Hosh logo");
+        signUp.add("Google Step 2 : Select Account you want as your personal login Account");
+        signUp.add("Google Step 3 : Fill in the details required by the app in the next page.");
+        signUp.add("Email Step 1 : Click on the Email Sign In button below the Hosh logo");
+        signUp.add("Email Step 2 : A verification would be sent to your mail.");
+        signUp.add("Email Step 3 : After verifying Fill in the details required by the app in the next page.");
+        signUp.add("Anonymous Step 1 : Click on the Anonymous Sign In button below the Hosh logo");
+        signUp.add("Anonymous Step 2 : Fill in the details required by the app in the next page.");
+        signUp.add("Anonymous Step 3 : The Catch here is, Once user signs out, All the details are gone");
 
         List<String> login = new ArrayList<>();
-        login.add("Once user is signed up, you are directed to another page where you will Login");
-        login.add("Step 1 : Click on the Google Sign In button below the logo");
-        login.add("Step 2 : Enter Your Password and click the login button");
+        login.add("Once user is signed up, logging in from another phone is quite easy.");
+        login.add("Step 1 : Click the sign in button you used to sign up and you are taken to your homepage.");
 
-        List<String> forgotPass = new ArrayList<>();
-        forgotPass.add("Step 1 : Click on the Forgot Password link");
-        forgotPass.add("Step 2 : Provide an answer to the Secure Question");
-        forgotPass.add("Step 3 : Enter the recovered password and login");
 
         List<String> homePage = new ArrayList<>();
-        homePage.add("The home page consist of (2) main Pages that can be accessed by swiping left or right");
-        homePage.add("1 : Courses Page");
-        homePage.add("Users can click on any of the courses they would love to explore");
-        homePage.add("2 : News Page");
-        homePage.add("Users can read up on the latest news and also add to the news feed simply by; ");
-        homePage.add("Step 1 : Click on the button at the bottom right of the news page");
-        homePage.add("Step 2 : Enter the full description and topic of the news");
-        homePage.add("Images can be included if available");
-        homePage.add("PLEASE NOTE : That both the topic and the description are censored so, please you are advised to use maturely");
+        homePage.add("The home page consist of (5) main Pages that can be accessed by clicking on the bottom navigation");
+        homePage.add("1. Hosh Feed, 2. Hookup, 3. Hopdate, 4. Messages, 5. Account");
+        homePage.add("1. Hosh Feed : This contains updates from every user on the Hosh App");
+        homePage.add("2. Hookup : This is a search medium to find other users close to you");
+        homePage.add("3. Hopdate : This is where you can add content to your timeline for everyone to see");
+        homePage.add("4. Messages : This contains messages to and from every contact you make.");
+        homePage.add("5. Account : This contains brief details about user and also timeline.");
 
-        List<String> coursesOptions = new ArrayList<>();
-        coursesOptions.add("Each course has (5) different options which include; ");
-        coursesOptions.add("1. Materials");
-        coursesOptions.add("Materials can be downloaded easily from the materials page");
-        coursesOptions.add("2. Audio Tutorials");
-        coursesOptions.add("Audio tutorials are audio lessons provided by the lecturer and can also be downloaded easily");
-        coursesOptions.add("3. Past Questions and Solutions");
-        coursesOptions.add("The Past Questions and solutions are also available for download on this page");
-        coursesOptions.add("4. Practice Test");
-        coursesOptions.add("Practice tests are online quiz users are allowed to take and scored immediately based on their performances");
-        coursesOptions.add("5. Group Chat");
-        coursesOptions.add("Group Chats are available to every user for different courses");
+        List<String> hoshFeed = new ArrayList<>();
+        hoshFeed.add("Each feed can be ;");
+        hoshFeed.add("1. Viewed for more details");
+        hoshFeed.add("2. Liked / Unliked");
+        hoshFeed.add("3. Commented on");
+        hoshFeed.add("4. Shared");
+        hoshFeed.add("5. Reported");
+        hoshFeed.add("6. Viewing Poster's Profile is also possible");
 
-        List<String> downloadedFiles = new ArrayList<>();
-        downloadedFiles.add("Downloaded Files can only be accessed through the SmartLinks App");
-        downloadedFiles.add("The downloaded files are accessible by;");
-        downloadedFiles.add("Step 1 : Click on the 3Stack icon on the top left part of the Home Page");
-        downloadedFiles.add("Step 2 : Click on Downloads");
+        List<String> hookup = new ArrayList<>();
+        hookup.add("The Search results on this page shows people you are interested in and is also based on proximity");
 
-        List<String> contactUs = new ArrayList<>();
-        contactUs.add("Users can contact us by sending us a mail directly from the map");
-        contactUs.add("Click on the 3Dot menu on the Home Page located at the top right corner of the Home Page");
-        contactUs.add("Step 1 : Click on the Help Menu");
-        contactUs.add("Step 2 : Click on Contact Us");
+        List<String> hopdate = new ArrayList<>();
+        hopdate.add("Hopdate can contain Image Only, Text Only or Both");
+
+        List<String> messaging = new ArrayList<>();
+        messaging.add("This contains all messages sent and received");
+
+        List<String> account = new ArrayList<>();
+        account.add("Here Users can find their details by clicking on the profile button and also make adjustments as they like");
+        account.add("Each user also have an image gallery where they can pick their profile pictures from");
+        account.add("Each user has a list of other users that are followers and they are following");
 
 
         listHash.put(listFaqTitle.get(0), signUp);
         listHash.put(listFaqTitle.get(1), login);
-        listHash.put(listFaqTitle.get(2), forgotPass);
-        listHash.put(listFaqTitle.get(3), homePage);
-        listHash.put(listFaqTitle.get(4), coursesOptions);
-        listHash.put(listFaqTitle.get(5), downloadedFiles);
-        listHash.put(listFaqTitle.get(6), contactUs);
+        listHash.put(listFaqTitle.get(2), homePage);
+        listHash.put(listFaqTitle.get(3), hoshFeed);
+        listHash.put(listFaqTitle.get(4), hookup);
+        listHash.put(listFaqTitle.get(5), hopdate);
+        listHash.put(listFaqTitle.get(6), messaging);
+        listHash.put(listFaqTitle.get(7), account);
 
     }
 }
