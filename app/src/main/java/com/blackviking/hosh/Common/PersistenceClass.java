7b7fb286573558858e3357400013fe9bdd6d2521
Adesuyi Ayodeji <28259980+Scarecrow94@users.noreply.github.com>
2019-02-20 22:06:54 +0100

    Notification Channels Setup

--- app/src/main/java/com/blackviking/hosh/Common/PersistenceClass.java
+++ app/src/main/java/com/blackviking/hosh/Common/PersistenceClass.java
@@ -1,5 +1,8 @@
 package com.blackviking.hosh.Common;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.support.multidex.MultiDexApplication;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,6 +14,10 @@ import com.squareup.picasso.Picasso;
  */
 
 public class PersistenceClass extends MultiDexApplication {
+
+    public static final String CHANNEL_1_ID = "messagingChannel";
+    public static final String CHANNEL_2_ID = "feedChannel";
+    public static final String CHANNEL_3_ID = "accountChannel";
 
     @Override
     public void onCreate() {
@@ -25,6 +32,45 @@ public class PersistenceClass extends MultiDexApplication {
         built.setIndicatorsEnabled(false);
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
+
+        createNotificationChannels();
+
+    }
+
+    private void createNotificationChannels() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Messaging",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.enableVibration(true);
+            channel1.enableLights(true);
+            channel1.setShowBadge(true);
+            channel1.setDescription("This is the Messaging Channel");
+
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Hosh Feed",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel2.setDescription("This is the Feed Channel");
+
+            NotificationChannel channel3 = new NotificationChannel(
+                    CHANNEL_3_ID,
+                    "Account",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel3.setDescription("This is the Account Channel");
+
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+            manager.createNotificationChannel(channel3);
+        }
 
     }
 }
